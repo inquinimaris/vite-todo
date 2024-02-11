3d0319b5792ea19990d1579b01413eff971431ab
Inquinimaris <inquinima@gmail.com>
2024-02-12 03:46:45 +0600

    new popups; fallthrough methods; tolerable styling;

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
       <div>
         <div v-if="tasks.length">
           <h3>Things to do:</h3>
-          <ul>
-            <the-task v-for="task in tasks" :key="task.id" :taskTitle="task.title" :taskDescr="task.descr"></the-task>
+          <ul class="flex flex-col gap-y-6 my-8">
+              <the-task v-for="task in tasks" :key="task.id" :taskId="task.id" :taskTitle="task.title" :taskDescr="task.descr" :togglePopupProp="togglePopup"></the-task>
           </ul>
         </div>
         <div v-else>
@@ -16,14 +16,14 @@
       </div>
       <!-- list area end -->
       <div class="flex flex-row justify-center items-center gap-x-8">
-        <button type="button" @click="toggleTaskCreatePopup()" class="border border-green-400 px-6 py-3">New task</button>
+        <button type="button" @click="togglePopup('create')" class="border border-green-400 px-6 py-3">New task</button>
       </div>
     </div>
   </main>
   <the-footer class="mt-12"></the-footer>
   <transition name="overlay">
-  <div @click.self="toggleTaskCreatePopup()" v-show="overlay" class="absolute top-0 left-0 w-full h-full bg-slate-800/40 grid place-items-center">
-    <div class="flex flex-col gap-4 bg-slate-400 p-6 shadow-xl">
+  <div @click.self="togglePopup()" v-show="overlay" class="fixed top-0 left-0 w-full h-full bg-slate-800/40 grid place-items-center">
+    <div v-show="popupCreate" class="flex flex-col gap-4 bg-slate-400 p-6 shadow-xl">
         <div class="flex flex-row justify-between gap-2">
           <label for="newTaskTitle">Title:</label>
           <input id="newTaskTitle" type="text" placeholder="Try to have fun" class="bg-slate-400 border-green-300 border">
@@ -32,7 +32,29 @@
           <label for="newTaskDescr">Description:</label>
           <input id="newTaskDescr" type="text" placeholder="Reasons? I dunno, no reasons" class="bg-slate-400 border-green-300 border">
         </div>
-        <button @click="taskCreate">Add new task</button>
+        <button type="button" @click="taskCreate">Add new task</button>
+    </div>
+
+    <div v-show="popupUpdate" class="flex flex-col gap-4 bg-slate-400 p-6 shadow-xl">
+      <div class="flex flex-row justify-between gap-2">
+          <label for="updateTaskTitle">New title:</label>
+          <input id="updateTaskTitle" type="text" placeholder="Leave empty" class="bg-slate-400 border-green-300 border">
+        </div>
+        <div class="flex flex-row justify-between gap-2">
+          <label for="updateTaskDescr">New description:</label>
+          <input id="updateTaskDescr" type="text" placeholder="Leave empty" class="bg-slate-400 border-green-300 border">
+        </div>
+        <button type="button" @click="taskUpdate">Update</button>
+    </div>
+
+    <div v-show="popupDelete" class="flex flex-col gap-4 bg-slate-400 p-6 shadow-xl">
+      <p>Delete task, for real?</p>
+      <button type="button" @click="taskDelete">Delete</button>
+    </div>
+
+    <div v-show="popupComplete" class="flex flex-col gap-4 bg-slate-400 p-6 shadow-xl">
+      <p>GJ~</p>
+      <button type="button" @click="taskComplete">I know right?</button>
     </div>
   </div>
   </transition>
@@ -44,6 +66,10 @@ export default{
     return{
       tasks:[],
       overlay: false,
+      popupCreate: false,
+      popupUpdate: false,
+      popupDelete: false,
+      popupComplete: false,
     }
   },
   methods:{
@@ -53,19 +79,42 @@ export default{
         title: document.getElementById('newTaskTitle').value,
         descr: document.getElementById('newTaskDescr').value
       });
-      this.toggleTaskCreatePopup();
+      this.togglePopup();
     },
     taskUpdate(){
-
+      console.info('kinda updated');
+      this.togglePopup();
     },
     taskDelete(){
-
+      console.info('kinda deleted');
+      this.togglePopup();
     },
     taskComplete(){
-
+      console.info('kinda completed');
+      this.togglePopup();
     },
-    toggleTaskCreatePopup(){
+    togglePopup(popupType){
       this.overlay = !this.overlay;
+      switch(popupType){
+        case 'create':
+          this.popupCreate = true;
+          break;
+        case 'update':
+          this.popupUpdate = true;
+          break;
+        case 'delete':
+          this.popupDelete = true;
+          break;
+        case 'complete':
+          this.popupComplete = true;
+          break;
+        default:
+          this.popupCreate = false;
+          this.popupUpdate = false;
+          this.popupDelete = false;
+          this.popupComplete = false;
+          break;
+      }
     }
   }
 }
--- src/components/TheTask.vue
+++ src/components/TheTask.vue
@@ -1,19 +1,27 @@
 <template>
-    <li class="border border-green-300">
-        <div>
-            <h4>{{ taskTitle }}</h4>
-            <p>{{ taskDescr }}</p>
-        </div>
-        <div class="flex flex-row gap-6">
-            <button type="button" class="bg-red-600 text-slate-700 px-6 py-3">Delete</button>
-            <button type="button" class="bg-blue-200 text-slate-700 px-6 py-3">Edit</button>
-            <button type="button" class="bg-green-400 text-slate-700 px-6 py-3">Complete</button>
-        </div>
-    </li>
+    <transition name="task" appear>
+        <li class="border border-green-300 p-4">
+            <div class="flex flex-col gap-y-4">
+                <h4 class="font-semibold text-xl">{{ taskTitle ? taskTitle : 'Untitled'}}</h4>
+                <p>{{ taskDescr ? taskDescr : 'No description' }}</p>
+                <p class="opacity-50 text-xs uppercase">Task id: {{ taskId }}</p>
+            </div>
+            <div class="flex flex-row justify-center items-center gap-6 max-w-lg mx-auto">
+                <button type="button" @click="togglePopup('delete')" class="bg-red-600 text-slate-700 px-6 py-3">Delete</button>
+                <button type="button" @click="togglePopup('update')" class="bg-blue-200 text-slate-700 px-6 py-3">Update</button>
+                <button type="button" @click="togglePopup('complete')" class="bg-green-400 text-slate-700 px-6 py-3">Complete</button>
+            </div>
+        </li>
+    </transition>
 </template>
 
 <script>
 export default{
-    props:['taskTitle', 'taskDescr'],
+    props:['taskId', 'taskTitle', 'taskDescr', 'togglePopupProp'],
+    methods:{
+        togglePopup(popupType){
+            this.togglePopupProp(popupType);
+        }
+    }
 }
 </script>
